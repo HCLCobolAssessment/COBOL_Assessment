9c23fba2a3bc7109ef72120b48bccafc6b50d3df
Administrator <Administrator@172.31.18.58>
2019-02-18 11:37:28 +0000

    demo sample 2

--- String_Functions2/Program2.cbl
+++ String_Functions2/Program2.cbl
@@ -18,7 +18,7 @@
        01 SPLIT3 PIC X(7).	   
 
        PROCEDURE DIVISION.
-       DISPLAY 'hi hi bye, **y***'.
+       DISPLAY 'hi hi bye, sample'.
        DISPLAY "Task1 = " xStr(9:3)
               *> count the number of chars in string, store in aNum     
               *>INSPECT yStr TALLYING aNum FOR ALL CHARACTERS.
