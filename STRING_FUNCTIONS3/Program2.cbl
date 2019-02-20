47ea745e76211b83d95fa50fc7dfad6ed9726553
Administrator <Administrator@172.31.18.58>
2019-02-20 08:49:12 +0000

    commit check

--- STRING_FUNCTIONS3/Program2.cbl
+++ STRING_FUNCTIONS3/Program2.cbl
@@ -29,7 +29,7 @@
               *> replace A chars with X in strings
               DISPLAY "OLD STRING : "yStr.
               INSPECT yStr REPLACING ALL 'L' BY 'X'.
-              DISPLAY "NEW STRING : "yStr.
+              DISPLAY "NEW STR : "yStr.
               DISPLAY "HAIIIII".
               *> string concatenate
               STRING str2 DELIMITED BY SIZE
